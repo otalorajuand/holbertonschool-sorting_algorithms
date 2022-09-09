54e3d0b809f5b2ec9283f453f82a66aad07d9110
otalorajuand <otalorajuand@gmail.com>
2022-09-09 00:38:15 +0000

    correction of task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,33 @@
 #include "sort.h"
 
 /**
+ * swap_list_nodes - Swaps two nodes from a linked list.
+ * @list: A double pointer to the first element of the list.
+ * @first_node: A pointer to the first element to swap.
+ * @second_node: A pointer to the second element to swap.
+* Return: Nothing.
+ */
+
+void swap_list_nodes(listint_t **list, listint_t *first_node,
+		listint_t *second_node)
+{
+	/* swap nodes */
+	first_node->next = second_node->next;
+	second_node->prev = first_node->prev;
+	first_node->prev = second_node;
+	second_node->next = first_node;
+	if (second_node->prev)
+	{
+		second_node->prev->next = second_node;
+	}
+	else
+		*list = second_node; /* need to change */
+			/* the first node of the list */
+	if (first_node->next)
+		first_node->next->prev = first_node;
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: The list to be sorted.
@@ -22,21 +49,7 @@ void insertion_sort_list(listint_t **list)
 		while ((tmp_prev != NULL) && (tmp->n < tmp_prev->n))
 		{
 			/* swap nodes */
-			tmp_prev->next = tmp->next;
-			tmp->prev = tmp_prev->prev;
-			tmp_prev->prev = tmp;
-			tmp->next = tmp_prev;
-			if (tmp->prev != NULL)
-			{
-				tmp->prev->next = tmp;
-			}
-			else
-				*list = tmp; /* need to change */
-						/* the first node of the list */
-
-			if (tmp_prev->next != NULL)
-				tmp_prev->next->prev = tmp_prev;
-
+			swap_list_nodes(list, tmp_prev, tmp);
 			print_list(*list);
 			tmp_prev = tmp->prev;
 		}
